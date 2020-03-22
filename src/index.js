e76436894324b1fddc97db21180b8d39958526f2
tanyushanel <tanyushanel@users.noreply.github.com>
2020-03-22 15:00:19 +0300

    feat: solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (!(str && bracketsConfig)) return false;
+
+    let stack = [];
+    let arr = str.split('');
+    let configToPlaneArr = bracketsConfig.join(',').split(','); // to 1 level arr
+    let configToPairs = bracketsConfig.map((item) => item.join('')); //to bracket pairs 
+
+    for (let i = 0; i < arr.length; i++) {
+        if (configToPlaneArr.includes(arr[i])) { // configArr includes bracket
+
+            stack.push(arr[i]);
+            isPair(stack, arr[i]);
+
+        } else return false;
+    }
+
+    return stack.length ? false : true;
+
+    function isPair(stack, bracket) {
+        let previous = stack[stack.length - 2]; // get previous open bracket      
+        let pair = previous + bracket;
+
+        if (configToPairs.includes(pair)) {
+            stack.pop();
+            stack.pop();
+        }
+
+    }
 }
